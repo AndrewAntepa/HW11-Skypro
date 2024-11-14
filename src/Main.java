8254207232f265869eacd59d17ed7302ffa15fe9
AndrewAntepa <andrewantepa@gmail.com>
2024-11-14 17:02:49 +0800

    hw11_2

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,11 @@ public class Main {
 
     public static String operationSystem(int year, int OS) {
         int currentYear = 2015;
-        String answer = "";
-        if (OS == 0) {
-            if (currentYear > year) {
-                answer = "Установите облегченную версию приложения для iOS по ссылке";
-            } else answer = "Установите версию приложения для iOS по ссылке";
-        } else if (OS == 1) {
-            if (currentYear > year) {
-                answer = "Установите облегченную версию приложения для Android по ссылке";
-            } else answer = "Установите версию приложения для Android по ссылке";
-        }
-        return answer;
+        String[][] answers = {
+                {"Установите облегченную версию приложения для iOS по ссылке", "Установите версию приложения для iOS по ссылке"},
+                {"Установите облегченную версию приложения для Android по ссылке", "Установите версию приложения для Android по ссылке"}
+        };
+        return answers[OS][currentYear > year ? 0 : 1];
     }
 
     public static int delivery(int distance) {
